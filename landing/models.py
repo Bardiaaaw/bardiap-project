d1f8431b843c9988a327c53f72d9b9ad9244341c
Bardiaaaw <https://github.com/Bardiaaaw>
2026-07-29 01:48:51 +0330

    Tarahi Jadiddd

--- landing/models.py
+++ landing/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Message(models.Model):
     name = models.CharField(max_length=100, verbose_name="نام و نام خانوادگی")
-    phone = models.CharField(max_length=15, verbose_name="شماره تماسو بده داشته باشیم")
+    phone = models.CharField(max_length=11, verbose_name="شماره تماسو بده داشته باشیم")
     message = models.TextField(verbose_name="پیام")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ارسال")
     
